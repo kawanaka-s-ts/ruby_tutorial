c10176802ddccb313d0874898cd2288cfb150322
kawanaka <kawanaka_s@tenso.com>
2018-12-24 20:06:25 +0900

    fixup! 4-10.rb
    - 別パターン追記
    　要素間半角スペース追加

--- 4-10.rb
+++ 4-10.rb
@@ -1,8 +1,12 @@
-#配列[1,2,3]の総和(1+2+3)をeachメソッドを使用して求める
+#配列[1, 2, 3]の総和(1+2+3)をeachメソッドを使用して求める
 
 sum = 0
-a = [1,2,3]
+a = [1, 2, 3]
 a.each do |x|
     sum = sum + x
 end
+puts sum
+
+sum = 0
+[1, 2, 3].each { |x| sum += x }
 puts sum
