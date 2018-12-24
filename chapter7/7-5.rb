a8a0711f14627df61e67cda52b4f3baa4a01e9bc
kawanaka <kawanaka_s@tenso.com>
2018-12-24 23:04:34 +0900

    fixup! 7-5.rb
    - 重複しない形に修正
    　インデント修正および空行追加

--- chapter7/7-5.rb
+++ chapter7/7-5.rb
@@ -1,8 +1,13 @@
-def dice
-   diceresult = [1, 2, 3, 4, 5, 6].sample
-   return diceresult unless diceresult == 1
-    puts "もう一回"
+def dice_shack
     [1, 2, 3, 4, 5, 6].sample
 end
 
+def dice
+    dice_shack
+    return dice_shack unless dice_shack == 1
+
+    puts "もう一回"
+    dice_shack
+end
+ 
 puts dice
