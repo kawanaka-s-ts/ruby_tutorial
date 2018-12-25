d2f86662128cda35d21aadf43841899b105c64dd
kawanaka <kawanaka_s@tenso.com>
2018-12-25 18:15:14 +0900

    fixup! fixup! fixup! 7-5.rb
    - 早期リターンを使用した形に書き換え
    　インデント修正

--- chapter7/7-5.rb
+++ chapter7/7-5.rb
@@ -1,15 +1,12 @@
-def dice_shack
+def roll_dice
   [1, 2, 3, 4, 5, 6].sample
 end
 
 def dice
-  result_num = dice_shack
-  if result_num == 1 
+  result_num = roll_dice
+  return roll_dice if result_num != 1
     puts "もう一回" 
-  return dice
-  else
     result_num
-  end
 end
  
 puts dice
