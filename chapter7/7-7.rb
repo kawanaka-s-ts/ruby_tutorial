6ed075afa7c672dcb5a098ed8c4d229311ae88da
kawanaka <kawanaka_s@tenso.com>
2018-12-25 09:23:53 +0900

    fixup! 7-7.rb
    - コーディングスタイル修正および、全ての組合せ追記

--- chapter7/7-7.rb
+++ chapter7/7-7.rb
@@ -1,7 +1,12 @@
-def price (item:, size:)
-   items = {"コーヒー" => 300,"カフェラテ" => 400}
-   sizes = {"ショート" => 0,"トール" => 50,"ベンティ" => 100}
+def price(item:, size:)
+   items = { "コーヒー" => 300, "カフェラテ" => 400 }
+   sizes = { "ショート" => 0, "トール" => 50, "ベンティ" => 100 }
    items[item] + sizes[size]
 end
 
+puts price(item:"コーヒー", size:"ショート")
 puts price(item:"コーヒー", size:"トール")
+puts price(item:"コーヒー", size:"ベンティ")
+puts price(item:"カフェラテ", size:"ショート")
+puts price(item:"カフェラテ", size:"トール")
+puts price(item:"カフェラテ", size:"ベンティ")
