4b41404f96c019ff7e226ea8a263776b9c5551ef
kawanaka <kawanaka_s@tenso.com>
2018-12-25 14:36:32 +0900

    fixup! fixup! 7-5.rb
    - 目的の動作を行うよう修正
    　インデント修正

--- chapter7/7-5.rb
+++ chapter7/7-5.rb
@@ -1,13 +1,15 @@
 def dice_shack
-    [1, 2, 3, 4, 5, 6].sample
+  [1, 2, 3, 4, 5, 6].sample
 end
 
 def dice
-    dice_shack
-    return dice_shack unless dice_shack == 1
-
-    puts "もう一回"
-    dice_shack
+  result_num = dice_shack
+  if result_num == 1 
+    puts "もう一回" 
+  return dice
+  else
+    result_num
+  end
 end
  
 puts dice
