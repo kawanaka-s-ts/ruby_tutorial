0f1c8b203b69860c98006bd75aade7dd15e400b9
kawanaka <kawanaka_s@tenso.com>
2018-12-26 10:49:59 +0900

    fixup! fixup! 2-7.rb
    - 変数名変更

--- 2-7.rb
+++ 2-7.rb
@@ -1,9 +1,9 @@
 #2-6からコーヒーが400円に値上がりしました
 #プログラムを書き換えてください
 
-x = 1
-y = 2
-a = 400 * x
-b = 100 * y
-puts "コーヒー:#{a}円"
-puts "合計:#{a + b}円"
+coffee = 1
+espresso = 2
+coffee_price = 400 * coffee
+espresso_price = 100 * espresso
+puts "コーヒー:#{coffee_price}円"
+puts "合計:#{coffee_price + espresso_price}円"
