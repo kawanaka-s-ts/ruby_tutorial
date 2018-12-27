22c340f5041202f7a9030b02cc93bd8bbb00bebb
kawanaka <kawanaka_s@tenso.com>
2018-12-27 10:22:38 +0900

    fixup! 10-3.rb
    - 代入と表示を分けて表記

--- chapter10/10-3.rb
+++ chapter10/10-3.rb
@@ -2,5 +2,6 @@ require "net/http"
 require "uri"
 require "cgi"
 uri = URI.parse("http://localhost:4567/drink")
-p result = Net::HTTP.get(uri)
+result = Net::HTTP.get(uri)
+p result
 p CGI.unescape(result)
